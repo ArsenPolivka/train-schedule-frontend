c3bbb8969a3b01c0f2cddee3fdd54af915169b3c
PolivkaArsen <arsen.polivka@tetalab.com>
2025-03-10 15:39:31 +0200

    middleware fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,14 +3,25 @@ import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
   const token = request.cookies.get('token')?.value;
+  const { pathname } = request.nextUrl;
 
-  if (request.nextUrl.pathname.startsWith('/api')) {
+  if (pathname.startsWith('/api')) {
     return NextResponse.next();
   }
 
-  if (request.nextUrl.pathname.startsWith('/login') && token) {
+  if (!token && pathname !== '/login') {
+    return NextResponse.redirect(new URL('/login', request.url));
+  }
+
+  if (token && pathname === '/login') {
     return NextResponse.redirect(new URL('/', request.url));
   }
 
   return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
+  ],
 }
