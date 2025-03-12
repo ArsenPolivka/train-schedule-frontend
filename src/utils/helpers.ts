0460b5b37183d6ce653d95a5411815c5925fb04b
PolivkaArsen <arsen.polivka@tetalab.com>
2025-03-13 01:10:13 +0200

    fix cookie function

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,6 +1,14 @@
 export const getCookie = (name: string) => {
+  if (typeof document === 'undefined') {
+    return null;
+  }
+
   const value = `; ${document.cookie}`;
   const parts = value.split(`; ${name}=`);
 
-  if (parts.length === 2) return parts.pop()?.split(';').shift();
+  if (parts.length === 2) {
+    return parts.pop()?.split(';').shift();
+  }
+
+  return null;
 };
